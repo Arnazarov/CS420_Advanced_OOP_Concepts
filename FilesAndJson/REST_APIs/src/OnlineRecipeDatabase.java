9d8031747980772145076b650ea13852fd71b503
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2021-06-24 21:58:42 -0500

    completed all requirements in REST_APIs description

--- FilesAndJson/REST_APIs/src/OnlineRecipeDatabase.java
+++ FilesAndJson/REST_APIs/src/OnlineRecipeDatabase.java
@@ -1,57 +1,116 @@
 import com.github.cliftonlabs.json_simple.JsonArray;
 import com.github.cliftonlabs.json_simple.JsonObject;
 import com.github.cliftonlabs.json_simple.Jsoner;
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
 public class OnlineRecipeDatabase {
 
     String baseUrl = "http://www.recipepuppy.com/api/?";
+    HttpsURLConnection connection;
 
     public JsonObject getRecipesByIngredients(String ingredients) throws Exception
     {
-        //Getting the things ready to connect to the web
-        URL url = new URL(baseUrl+"i="+ingredients);
-/* TODO 
-You have to use the url to retrieve the contents of the website. 
-This will be a String, but in JSON format. */
-        return /* TODO 
-Remember to return a JSON object.*/;
+        BufferedReader reader;
+        String line;
+        StringBuffer responseContent = new StringBuffer();
+
+        try {
+            URL url = new URL(baseUrl+"i="+ingredients);
+            connection = (HttpsURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            int status = connection.getResponseCode();
+
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while ((line = reader.readLine()) != null) {
+                responseContent.append(line);
+            }
+
+            reader.close();
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            connection.disconnect();
+        }
+
+        JsonObject doc = (JsonObject) Jsoner.deserialize(responseContent.toString(), new JsonObject());
+
+        return doc;
     }
 
     public JsonObject getRecipesByDish(String dish) throws Exception
     {
         //Getting the things ready to connect to the web
-        /* TODO 
-Fill in this data type (Object) */ url = new /* TODO
-Fill in this datatype (Object) */(baseUrl+"q="+dish);
+        BufferedReader reader;
+        String line;
+        StringBuffer responseContent = new StringBuffer();
 
-       /* TODO
-Read the information coming from the web
- */
-        return /* TODO 
-return the appropriate result.
-*/
+        try {
+            URL url = new URL(baseUrl+"q="+dish);
+            connection = (HttpsURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            //int status = connection.getResponseCode();
+
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while ((line = reader.readLine()) != null) {
+                responseContent.append(line);
+            }
+
+            reader.close();
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            connection.disconnect();
+        }
+
+        JsonObject doc = (JsonObject) Jsoner.deserialize(responseContent.toString(), new JsonObject());
+
+        return doc;
     }
 
 
     public String formatRecipeAsString(JsonObject doc){
         String results = "";
-        /* TODO 
-This should return a String with each recipe in three lines:
-Title:the title of the recipe
-Link:the link to the recipe
-Ingredients:The ingredients of teh recipe.*/
+        JsonArray recipes = (JsonArray) doc.get("results");
+        for (int i = 0; i < recipes.size(); i++) {
+            JsonObject recipe = (JsonObject) recipes.get(i);
+            results += (String) recipe.get("title") + "\n";
+            results += (String) recipe.get("href") + "\n";
+            results += (String) recipe.get("ingredients") + "\n";
+        }
         return results;
     }
 
     public void saveRecipes(String text, String outfile){
-        /* TODO
-Given a String with some text in it, write that text to a file. 
-The name of the file is given in outfile */
+        try (FileOutputStream fos = new FileOutputStream(outfile);
+             DataOutputStream dos = new DataOutputStream(fos);) {
+
+            dos.writeChars(text);
+
+        }  catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 
 }
