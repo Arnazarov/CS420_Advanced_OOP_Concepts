8485201c16843169a028de8573b681d9faef86e3
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2021-07-12 20:31:54 -0500

    completed all requirements in RacingCars description

--- ThreadsAndConcurrency/RacingCars/src/Racer.java
+++ ThreadsAndConcurrency/RacingCars/src/Racer.java
@@ -1,12 +1,23 @@
-public class Racer /* TODO 
-Add something here so that you can pass this Racer to the constructor of Thread*/{
+public class Racer implements Runnable {
     Car c;
     int distance = 1000; // this is a racer that is going to run 1000 miles.
 
-    /* TODO 
-Create a constructor that receives a Car objec and initializes c.*/
+    public Racer(Car car) {
+        this.c = car;
+    }
     @Override
-    /* TODO 
-Override one method.  This is the task the thread will perform... You have to determine which method this is.
-Check the method description. */
+    public void run()  {
+        while (c.odometer < distance) {
+            if (c.miles == 0) {
+                System.out.println(c.name + "is charging...");
+                c.charge();
+                try {
+                    Thread.sleep((long) c.maxMiles);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            } else
+                c.move(1);
+        }
+    }
 }
