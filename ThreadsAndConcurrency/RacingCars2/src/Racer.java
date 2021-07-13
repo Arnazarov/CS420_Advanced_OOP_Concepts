69cdb6287dafdec70114a3981e5264b9b7272683
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2021-07-12 20:32:16 -0500

    completed all requirements in RacingCars2 description

--- ThreadsAndConcurrency/RacingCars2/src/Racer.java
+++ ThreadsAndConcurrency/RacingCars2/src/Racer.java
@@ -1,3 +1,23 @@
-public class Racer /* TODO 
-This class is exactly as the Racer from Task 1*/
+public class Racer implements Runnable {
+    Car c;
+    int distance = 1000; // this is a racer that is going to run 1000 miles.
+
+    public Racer(Car car) {
+        this.c = car;
+    }
+    @Override
+    public void run()  {
+        while (c.odometer < distance) {
+            if (c.miles == 0) {
+                System.out.println(c.name + "is charging...");
+                c.charge();
+                try {
+                    Thread.sleep((long) c.maxMiles);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            } else
+                c.move(1);
+        }
+    }
 }
