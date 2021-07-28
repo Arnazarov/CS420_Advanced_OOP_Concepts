10e29cccdd70892c68c6cce2a42ebd8eb8298f0f
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2021-07-27 20:36:07 -0500

    completed all requirements in Calculator description

--- InnerClassesAndLambdas/Calculator/src/Task.java
+++ InnerClassesAndLambdas/Calculator/src/Task.java
@@ -2,20 +2,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Task {
-/* TODO */
-Create a public static inner class called Triplet with generics for a first, second and third element. All variables should be publec.
+    public static class Triplet<N, E, S> {
+        public N n;
+        public E e;
+        public S s;
+        public Triplet(N n, E e, S s) {
+            this.n = n;
+            this.e = e;
+            this.s = s;
+        }
 
-    public static String result;
+    }
+    public static String result = "";
     public static void main(String[] args){
         Calculator c = new Calculator();
         List<Triplet<Double,Double,String>> t = new ArrayList<>();
         t.add(new Triplet<>(3.4,5.2,"+"));
         t.add(new Triplet<>(2.3,1.23,"-"));
-/* TODO */
-Add the code to add a multiplication of 4.5 x 5.4, a division by zero, and divide 56.0/28.0
+        t.add(new Triplet<>(4.5,5.4,"*"));
+        t.add(new Triplet<>(1.0,0.0,"/"));
+        t.add(new Triplet<>(56.0,28.0,"/"));
 
-        t.forEach(/* TODO */
-Utilizing a lambda expression, use the calculator to compute the operation specified in each Triplet with the corresponding numbers.);
+        t.forEach(x -> result += (c.compute(x.n, x.e, x.s).toString()) + "\n");
         System.out.println(result);
     }
 }
